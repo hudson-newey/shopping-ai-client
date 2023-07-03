ecfccb2185dd826c4213485b47f9b616f240634f
hudson-newey <neweyh@qut.edu.au>
2023-07-03 22:04:45 +1000

    Added metadata headers
    Fixes: #16

---
+++ inject_static.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# injecting static files into the dist container allows us to do better SEO performance
+# and add additional third party plugins
+
+cp ./static/* ./dist/;
--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'http://3.27.84.218/api',
-  brandName: 'Online Shopper GPT',
+  brandName: 'Online Shop GPT',
   affiliateCode: "shoppingas09c-20",
+  url: "http://onlineshopgpt.com",
 };
