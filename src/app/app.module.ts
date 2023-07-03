6b2a3faa7c87701bca8e552aa928ff7631f395d3
hudson-newey <neweyh@qut.edu.au>
2023-07-04 00:10:06 +1000

    added pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule, isDevMode } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -10,6 +10,7 @@ import { ChatMessageComponent } from "./components/chat-message/chat-message.com
 import { ChatHistoryComponent } from "./components/saved-chats/chat-history.component";
 import { SavedChatComponent } from "./components/saved-chat/saved-chat.component";
 import { ExamplesComponent } from "./components/examples/examples.component";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,13 @@ import { ExamplesComponent } from "./components/examples/examples.component";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
