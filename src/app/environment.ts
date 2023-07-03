521d8ff4c6b2c62603e8753fd433ca9db26b6b38
hudson-newey <neweyh@qut.edu.au>
2023-07-03 19:13:04 +1000

    added google analytics

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://3.27.84.218',
+  apiUrl: 'http://3.27.84.218/api',
   brandName: 'Online Shopper GPT',
   affiliateCode: "shoppingas09c-20",
 };
